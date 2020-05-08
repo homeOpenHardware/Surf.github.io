4ccae65ff697e5ff7a43c3f05a5a356ee74cd788
Aleksandr29071986 <33297826+Aleksandr29071986@users.noreply.github.com>
2020-05-08 22:56:06 +0300

    pluse

--- js/main.js
+++ js/main.js
@@ -100,7 +100,7 @@ new WOW().init();
 });
 
 
-$('<div class="quantity-nav"><div class="quantity-button quantity-up"><img src="img/plus.svg" alt=""></div><div class="quantity-button quantity-down"><img src="img/Minus.svg" alt=""></div></div>').insertAfter('.quantity input');
+$('<div class="quantity-nav"><div class="quantity-button quantity-up"><img src="img/plus.png" alt=""></div><div class="quantity-button quantity-down"><img src="img/Minus.svg" alt=""></div></div>').insertAfter('.quantity input');
 $('.quantity').each(function() {
   var spinner = $(this),
     input = spinner.find('input[type="number"]'),
